2995b577bbd6e312af34bb1743b2e18679d1370f
JackM510 <37175625+JackM510@users.noreply.github.com>
2026-09-22 21:55:38 +1000

    added template navigation

---
+++ src/components/Navigation.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <nav>
+        <ul class="flex py-4 px-12 gap-6 bg-blue-900 text-white">
+            <li>
+                <a href="" class="transition-opacity duration-200 hover:opacity-80">Home</a>
+            </li>
+            <li>
+                <a href="" class="transition-opacity duration-200 hover:opacity-80">About</a>
+            </li>
+            <li>
+                <a href="" class="transition-opacity duration-200 hover:opacity-80">Skills</a>
+            </li>
+            <li>
+                <a href="" class="transition-opacity duration-200 hover:opacity-80">Projects</a>
+            </li>
+            <li>
+                <a href="" class="transition-opacity duration-200 hover:opacity-80">Contact</a>
+            </li>
+        </ul>
+    </nav>
+</template>
