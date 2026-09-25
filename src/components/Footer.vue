af2221668fcf74bf20f1ee0dd7bc05b23c6380ce
Jack Marshall <37175625+JackM510@users.noreply.github.com>
2026-09-26 08:45:52 +1000

    updated footer with FA icon imports and responsive design

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faVuejs, faTypescript, faTailwindCss } from '@fortawesome/free-brands-svg-icons'
 </script>
 
 <template>
-    <div class="h-[70px] bg-black flex justify-left items-center text-white px-12">
+    <div class="min-h-[70px] flex flex-col sm:flex-row items-center 
+        bg-black text-white py-4 px-12">
         <div class="flex items-center gap-6">
+            <FontAwesomeIcon :icon="faVuejs" class="icon"/>
+            <FontAwesomeIcon :icon="faTypescript" class="icon" />
+            <FontAwesomeIcon :icon="faTailwindCss" class="icon" />
         </div>
-        <div class="ml-auto">
-            <span>Jack Marshall 2026</span>
+        <div class="ml-0 pt-4 sm:ml-auto sm:pt-0 lg:ml-0 lg:absolute lg:left-1/2 lg:-translate-x-1/2">
+            <span>© 2026 Jack Marshall</span>
         </div>
     </div>
 </template>
