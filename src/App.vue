56f29115c320ce83ae4f536c9e21da84525e11da
JackM510 <37175625+JackM510@users.noreply.github.com>
2026-09-26 07:38:52 +1000

    added basic footer component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import Navigation from './components/Navigation.vue';
 import Hero from './components/Hero.vue';
+import Footer from './components/Footer.vue';
 import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
   <Navigation />
   <Hero />
+  <Footer />
   <HelloWorld />
 </template>
---
+++ src/components/Footer.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="h-[70px] bg-black flex justify-left items-center text-white px-12">
+        <div class="flex items-center gap-6">
+        </div>
+        <div class="ml-auto">
+            <span>Jack Marshall 2026</span>
+        </div>
+    </div>
+</template>
