9957570e7769f74d9bf478cd180f562c13cebd14
JackM510 <37175625+JackM510@users.noreply.github.com>
2026-09-25 21:35:02 +1000

    added template hero section

---
+++ src/components/Hero.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+    import hero from "../assets/hero.jpg";
+</script>
+
+<template>
+    <div class="flex h-[calc(100vh-50px)] p-12">
+        <div 
+            class="flex justify-between mx-auto border rounded-2xl border-gray-700">
+            <div class="w-1/2 flex flex-col justify-center text-white text-left p-8">
+                <h1>Jack Marshall</h1>
+                <h2>Web Developer</h2>
+                <h2>Northern Rivers, NSW</h2>
+            </div>
+            <div class="w-1/2 aspect-[1/1]">
+                <img :src="hero" class="w-full h-full object-cover rounded-r-lg" alt="A picture of Jack Marshall" />
+            </div>
+        </div>
+    </div>
+</template>
