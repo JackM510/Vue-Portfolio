57f2d4bc6394360b7bb8a8c05c7d7a3a766da2d1
JackM510 <37175625+JackM510@users.noreply.github.com>
2026-09-25 19:21:39 +1000

    added mobile navigation and icons to socials

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Navigation from './components/Navigation.vue';
+import Hero from './components/Hero.vue';
 import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
   <Navigation />
+  <Hero />
   <HelloWorld />
 </template>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,9 +1,43 @@
 <script setup lang="ts">
+    import { ref, onMounted, onUnmounted } from 'vue';
+    import { Menu } from '@lucide/vue';
+    import github from '../assets/github.svg';
+    import linkedin from '../assets/linkedin.svg';
+
+    /* --- Handle Resizing --- */
+    const isOpen = ref(false); 
+    const handleResize = () => {
+        if (window.innerWidth >= 768) {
+            isOpen.value = false;
+        }
+    };
+    onMounted(() => {
+        window.addEventListener('resize', handleResize);
+    });
+    onUnmounted(() => {
+        window.removeEventListener('resize', handleResize);
+    });
+
 </script>
 
 <template>
-    <nav>
-        <ul class="flex py-4 px-12 gap-6 bg-blue-900 text-white">
+    <nav class="
+        h-[50px] sticky top-0 z-50 flex items-center px-6 md:px-12 gap-6 
+        bg-black text-white
+        opacity-40 transition-opacity duration-400 hover:opacity-100"
+    >  
+        <button
+            @click="isOpen = !isOpen"
+            class="md:hidden w-7 h-7 transition-opacity duration-200 hover:opacity-80"
+        >
+            <Menu />
+        </button>
+        <ul :class="['md:flex md:static md:gap-6 md:pb-0',
+                isOpen 
+                    ? 'flex flex-col absolute w-full top-[50px] left-0 bg-black gap-y-4 pb-2'
+                    : 'hidden'
+            ]"
+        >
             <li>
                 <a href="" class="transition-opacity duration-200 hover:opacity-80">Home</a>
             </li>
@@ -20,5 +54,13 @@
                 <a href="" class="transition-opacity duration-200 hover:opacity-80">Contact</a>
             </li>
         </ul>
+        <div class="ml-auto flex gap-6">
+            <a href="https://github.com/JackMarshall97/">
+                <img :src="github" class="w-8 h-8 transition-opacity duration-200 hover:opacity-80" alt="GitHub logo" />
+            </a>
+            <a href="https://www.linkedin.com/in/jackmarshall97/">
+                <img :src="linkedin" class="w-8 h-8 transition-opacity duration-200 hover:opacity-80" alt="LinkedIn logo" />
+            </a>
+        </div> 
     </nav>
 </template>
