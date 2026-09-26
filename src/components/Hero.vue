1363a36f03986fdee4d53e5959fd5cb46423511f
JackM510 <37175625+JackM510@users.noreply.github.com>
2026-09-26 21:16:35 +1000

    updated hero section height to prevent overflow

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -6,31 +6,30 @@
 <template>
     <div class="h-[calc(100vh-50px)] md:py-8">
         <div 
-            class="w-full md:w-4/5 xl:w-3/4
+            class="h-full w-full md:w-4/5 xl:w-3/4
                 flex flex-col md:flex-row md:mx-auto 
                 border rounded-3xl border-gray-700 md:p-2"
         >
-            <div class="w-1/2 flex flex-col basis-1/2 justify-center order-2 md:order-1 text-white text-left p-8">
+            <div class="flex flex-col md:basis-1/2 justify-center text-white text-left p-8">
                 <h1 class="uppercase">Jack Marshall</h1>
-                <div class="flex mb-8">
-                    <div class="flex-1 text-xl">
+                <div class="flex gap-8 mb-6">
+                    <div class="text-xl">
                         <FontAwesomeIcon :icon="faBriefcase" class="text-blue-400 mr-2"/> 
                         <span>Web Developer</span>
                     </div>
-                    <div class="flex-1 text-xl">
+                    <div class="text-xl">
                         <FontAwesomeIcon :icon="faLocationDot" class="text-red-400 mr-2"/> 
                         <span>Northern Rivers, NSW</span>
                     </div>
                 </div>
                 <hr class="text-gray-700"><br>
-                
-                
-                
-                <p class="text-2xl font-bold">Welcome to my personal website!</p>
-                
+                <p class="text-2xl font-bold pb-2">Welcome to my personal website!</p>
+                <p class="text-xl">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vitae justo nec sapien tincidLorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </p>
             </div>
-            <div class="basis-1/2 relative aspect-[1/1] order-1 md:order-2">
-                <div class="absolute inset-0 bg-black/20"></div>
+            <div class="relative md:basis-1/2 aspect-square">
+                <div class="absolute inset-0 bg-black/20 md:rounded-r-3xl"></div>
                 <img :src="hero" class="w-full h-full object-cover md:rounded-r-3xl" alt="A picture of Jack Marshall" />
             </div>
         </div>
